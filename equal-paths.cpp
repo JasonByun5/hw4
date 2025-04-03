1134536880f459c7b7c9ad9284a57fc982f62e74
Jason Byun <jsbyun@usc.edu>
2025-04-03 01:37:56 +0000

    Finished Tree Traversals

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,37 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool helper(Node * root, int depth, int &leafDepth);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    
+    int leafDepth = -1;
+    
+    return helper(root, 0, leafDepth);
+   
+}
+
+bool helper(Node * root, int depth, int &leafDepth){
+  
+  if (root == nullptr){
+    return true;
+  }
+
+  if ((root->left == nullptr && root->right == nullptr)){
+    if(leafDepth == -1){
+      leafDepth = depth;
+    }
+    return depth == leafDepth; //compares to other lengths of leaves
+  }
+  
+
+  return helper(root -> left, depth + 1, leafDepth)
+    && helper(root->right, depth + 1, leafDepth);
+
+  
+
 
 }
 
